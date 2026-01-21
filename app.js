fe38b7ad8bf9cb559624006c64b39e86a5dbbebd
Mir Md Mabrur <96931879+mabrur007@users.noreply.github.com>
2026-01-21 22:39:19 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+app.listen(port, () => {
+	console.log("server running on...", port);
+});
+
+app.get("/", (req, res) => {
+	res.send("hello world");
+});
+
+app.get("/about", (req, res) => {
+	res.send("this is about route");
+});
+
+app.get("/contact", (req, res) => {
+	res.send("this is contact route");
+});
+
+app.get("/portfolio", (req, res) => {
+	res.send("this is portfolio route");
+});
